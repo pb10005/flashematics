f0f7a553b9607336c13f17af44136529687e0c94
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2021-08-29 21:21:05 +0900

    Update PWA manifest

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,21 +2,21 @@ export default {
   // target: 'static',
   ssr: false,
   head: {
-    title: 'flashematics',
+    title: "flashematics",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: 'description',
-        name: 'description',
-        content: 'Flash cards'
+        hid: "description",
+        name: "description",
+        content: "Flash cards"
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     script: [
       {
         src:
-          'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-AMS-MML_SVG'
+          "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-AMS-MML_SVG"
       }
     ]
   },
@@ -24,12 +24,14 @@ export default {
   components: true,
   modules: [],
 
-  buildModules: ['@nuxtjs/vuetify', '@nuxtjs/pwa'],
+  buildModules: ["@nuxtjs/vuetify", "@nuxtjs/pwa"],
   pwa: {
     manifest: {
-      name: 'Flashematics',
-      lang: 'ja',
-      display: 'standalone'
+      name: "Flashematics",
+      short_name: "Flashematics",
+      theme_color: "4a4a4a",
+      lang: "ja",
+      display: "standalone"
     }
   },
   css: []
